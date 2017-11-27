4efc81c9e92da44d28ca8a2fd0ac44d2f5236133
Jan Keil <New Owner@2V1-HP>
2017-11-27 23:54:46 +0100

    test

---
+++ src/main/java/cz/uk/mff/keil/bioinfotool/FASTA_Parser.java
@@ -0,0 +1,19 @@
+package cz.uk.mff.keil.bioinfotool;
+
+import java.io.File;
+
+/**
+ *
+ * @author Jan Keil
+ */
+public class FASTA_Parser {
+
+    public FASTA_Parser() {
+        
+    }
+    
+    public FASTA_Parser(File file) {
+        
+    }
+    
+}
